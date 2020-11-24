13ee547c69ab35a6fa8d2ca623616ff69052ed0f
Gabriel <gabdanq@gmail.com>
2020-11-24 22:12:16 +0000

    setting ngrok process on parent process flask  reloader rather than flask dev server child process

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    START_NGROK = os.environ.get('START_NGROK', default=True)
+    START_NGROK = os.environ.get('START_NGROK') is not None and \
+    os.environ.get('WERKZEUG_RUN_MAIN') != 'true'
+
